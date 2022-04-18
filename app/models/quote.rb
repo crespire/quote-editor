2c8cadbf50852c0fc0b3f8e84aabadb34a63064c
crespire <li.simmon@gmail.com>
2022-04-18 11:31:19 -0400

    Update callbacks on Quote broadcast

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,6 +3,5 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> { broadcast_prepend_to 'quotes' }
-  after_update_commit -> { broadcast_replace_to 'quotes' }
+  broadcasts_to ->(quote) { 'quotes' }, inserts_by: :prepend
 end
